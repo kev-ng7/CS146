ec051446113825f9afc04de0e9c59c4eb21fa072
Kevin Nguyen <150320287+kev-ng7@users.noreply.github.com>
2024-04-30 21:45:38 -0700

    Add files via upload

---
+++ ExtraCredit/HW4/HW4.java
@@ -0,0 +1,21 @@
+public class HW4 {
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {3, 11, 6, 2, 9, 8, 5, 1, 13};
+        insertionSort(array);
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+}
