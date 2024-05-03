87cb006c6852b24ffaa18ddbcbc9fe69df33c0ef
Kevin Nguyen <150320287+kev-ng7@users.noreply.github.com>
2024-05-02 21:32:45 -0700

    Add files via upload

---
+++ Labs/Lab 8/Lab8.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Lab8 {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        if (dp[amount] > amount) {
+            return -1;
+        } else {
+            return dp[amount];
+        }
+    }
+}
