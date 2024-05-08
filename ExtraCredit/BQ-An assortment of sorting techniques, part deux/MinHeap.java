c2ca95220683282cbf7e34b80467afbdb1a7c6f8
Kevin Nguyen <150320287+kev-ng7@users.noreply.github.com>
2024-05-07 20:52:26 -0700

    Add files via upload

---
+++ ExtraCredit/BQ-An assortment of sorting techniques, part deux/MinHeap.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MinHeap {
+    public static void main(String[] args) {
+        Queue<Integer> heap = new PriorityQueue<>();
+
+        heap.offer(50);
+        heap.offer(11);
+        heap.offer(3);
+        heap.offer(25);
+        heap.offer(8);
+        heap.offer(37);
+
+        while (!heap.isEmpty()) {
+            System.out.print(heap.poll() + " ");
+        }
+    }
+}
